f7c34615671fbf8b158f3154ff75129458553646
Mehul Khandhadiya <53016215+mehulk-5245@users.noreply.github.com>
2023-07-12 13:14:45 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,12 +1,17 @@
-#suggest some better approach if possible
-from flask import Flask, request
+import sqlite3
 
-app = Flask(__name__)
+def login(username, password):
+    conn = sqlite3.connect("mydatabase.db")
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    conn.close()
+    if result:
+        print("Login successful!")
+    else:
+        print("Invalid credentials.")
 
-@app.route('/')
-def hello():
-    name = request.args.get('name')
-    return '<h1>Hello, ' + name + '!</h1>'
-
-if __name__ == '__main__':
-    app.run()
+username = input("Username: ")
+password = input("Password: ")
+login(username, password)
